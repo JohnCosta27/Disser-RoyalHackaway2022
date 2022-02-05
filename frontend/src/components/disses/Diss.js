0a784f930cfdd6b7023ea17f7a5382eedbd58a2b
Shrizy <shrijeetshrey77@gmail.com>
2022-02-05 13:15:35 +0000

    Disses

---
+++ frontend/src/components/disses/Diss.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Diss = ({diss}) => {
+  return (
+    <div className = "w-1/2 h-48 bg-blue-100 flex justify-center">
+      <h1 className = "text-2xl">{diss}</h1>
+      </div>
+  )
+}
+export default Diss;
