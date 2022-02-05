0c1bafa62f7c1ec4fc5fd2daae4b665678cd5281
Shrizy <shrijeetshrey77@gmail.com>
2022-02-05 14:03:46 +0000

    Disses

--- frontend/src/components/disses/Diss.js
+++ frontend/src/components/disses/Diss.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Diss = ({diss}) => {
+const Diss = ({name, diss}) => {
   return (
-    <div className = "w-1/2 h-48 bg-blue-100 flex justify-center">
-      <h1 className = "text-2xl">{diss}</h1>
-      </div>
+    <div className = "w-1/2 h-48 bg-blue-100 flex flex-col">
+      <h1 className = "text-4xl">{name}</h1>
+      <h1 className = "text-2xl text-gray-900 bg-zinc-300">{diss}</h1>
+    </div>
   )
 }
 export default Diss;
