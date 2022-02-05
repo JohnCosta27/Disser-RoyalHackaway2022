73ca9916a36fe39e38feccab96ce48e0ab42f849
Shrizy <shrijeetshrey77@gmail.com>
2022-02-05 16:02:51 +0000

    Button put on the bottom right of text area

---
+++ frontend/src/components/disses/Disser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Disser = () => {
+return (
+<div class="flex items-end justify-end fixed bottom-0 right-0 mb-4 mr-4 z-10">
+    <div>
+        <div className ="Follow me" target="_blank" class="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12">
+    </div>
+</div>
+</div>
+
+)
+}
