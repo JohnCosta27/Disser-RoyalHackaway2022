c345a345ca90004bf47e8b118dda2b7d38483a90
Shrizy <shrijeetshrey77@gmail.com>
2022-02-05 21:12:39 +0000

    Prettier

--- frontend/src/components/ResponseView.js
+++ frontend/src/components/ResponseView.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import Diss from './disses/Diss';
+import React from "react";
+import Diss from "./disses/Diss";
 const ResponseView = () => {
   return (
-    <div className = "w-full grid grid-cols-3 gap-4">
+    <div className="w-full grid grid-cols-3 gap-4">
       <div className="w-full flex flex-col">
-      <Diss diss="Diss 1.0" name="Shrey" />
-      <Diss diss="Diss 2.0" name="John" />
-      <Diss diss="Diss 3.0" name="James" />
-      <Diss diss="Diss 4.0" name="Dawg" />
-      </div>
-      <div className="w-full flex flex-col">
-      <Diss diss="Diss 1.0" name="Shrey" />
-      <Diss diss="Diss 2.0" name="John" />
-      <Diss diss="Diss 3.0" name="James" />
-      <Diss diss="Diss 4.0" name="Dawg" />
+        <Diss diss="Diss 1.0" name="Shrey" />
+        <Diss diss="Diss 2.0" name="John" />
+        <Diss diss="Diss 3.0" name="James" />
+        <Diss diss="Diss 4.0" name="Dawg" />
       </div>
       <div className="w-full flex flex-col">
-      <Diss diss="Diss 1.0" name="Shrey" />
-      <Diss diss="Diss 2.0" name="John" />
-      <Diss diss="Diss 3.0" name="James" />
-      <Diss diss="Diss 4.0" name="Dawg" />
+        <Diss diss="Diss 1.0" name="Shrey" />
+        <Diss diss="Diss 2.0" name="John" />
+        <Diss diss="Diss 3.0" name="James" />
+        <Diss diss="Diss 4.0" name="Dawg" />
       </div>
       <div className="w-full flex flex-col">
-      <Diss diss="Diss 1.0" name="Shrey" />
-      <Diss diss="Diss 2.0" name="John" />
-      <Diss diss="Diss 3.0" name="James" />
-      <Diss diss="Diss 4.0" name="Dawg" />
+        <Diss diss="Diss 1.0" name="Shrey" />
+        <Diss diss="Diss 2.0" name="John" />
+        <Diss diss="Diss 3.0" name="James" />
+        <Diss diss="Diss 4.0" name="Dawg" />
       </div>
+      <div className="w-full flex flex-col">
+        <Diss diss="Diss 1.0" name="Shrey" />
+        <Diss diss="Diss 2.0" name="John" />
+        <Diss diss="Diss 3.0" name="James" />
+        <Diss diss="Diss 4.0" name="Dawg" />
       </div>
-      )
-}
+    </div>
+  );
+};
 export default ResponseView;
