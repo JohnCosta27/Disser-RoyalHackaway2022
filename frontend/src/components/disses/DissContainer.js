6ff863182c85316da5a1077a7a50cc01c7e3aa0d
John Costa <johncosta027@gmaiil.com>
2022-02-05 21:14:24 +0000

    Small changes

--- frontend/src/components/disses/DissContainer.js
+++ frontend/src/components/disses/DissContainer.js
@@ -14,7 +14,7 @@ const DissContainer = () => {
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-snow-storm-100">
-      <h1 className="text-7xl font-bold text-center">Venture</h1>
+      <h1 className="text-7xl font-bold text-center">Ventur</h1>
       <div className="w-4/5 grid grid-cols-3 gap-2 items-center p-4">
         <Feed />
         {disses.map((diss) => (
