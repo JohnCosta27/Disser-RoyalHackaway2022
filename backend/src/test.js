0ec730ba264420524b76853d69520965da847d30
John Costa <johncosta027@gmaiil.com>
2022-02-05 16:02:22 +0000

    Added login method.

--- backend/src/test.js
+++ backend/src/test.js
@@ -2,9 +2,14 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const test = await prisma.disses.create({
-  data: {
-    diss: 'testing many to many',
-    userId: 'a5eb24f8-ecef-40b3-8491-a386e9839dad',
-  },
-});
+prisma.disses
+  .create({
+    data: {
+      diss: 'This is a reply to testing #1',
+      userId: 'a5eb24f8-ecef-40b3-8491-a386e9839dad',
+      originalDiss: '51a8b089-ff61-43b0-b312-3953a823347b',
+    },
+  })
+  .then((response) => {
+    console.log(response);
+  });
