d8e36ce53f9e783caccf53fff6812e69198ea45f
John Costa <johncosta027@gmaiil.com>
2022-02-05 11:22:25 +0000

    Added backend files.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-    <h1 className="text-3xl">Hello World</h1>
+      <h1 className="text-3xl">Hello World</h1>
     </div>
   );
 };
