0f7db6fa0240138a36d9c17b6dbb39d12596d850
Shrizy <shrijeetshrey77@gmail.com>
2022-02-06 00:46:09 +0000

    Like and Share

--- frontend/src/components/disses/Diss.js
+++ frontend/src/components/disses/Diss.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import clsx from 'clsx';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import clsx from "clsx";
+import { useNavigate } from "react-router-dom";
+import { ShareIcon } from "@heroicons/react/solid";
+import { HeartIcon } from "@heroicons/react/outline";
 
 const Diss = ({ id, name, diss, className }) => {
   const navigate = useNavigate();
@@ -8,12 +10,20 @@ const Diss = ({ id, name, diss, className }) => {
   return (
     <div
       className={clsx(
-        'w-full h-48 flex flex-col shadow-lg rounded bg-white p-4 cursor-pointer',
+        "w-full h-48 flex flex-col shadow-lg rounded bg-white p-4 cursor-pointer",
         className
       )}
-      onClick={() => navigate(`/response?dissId=${id}`)}>
+      onClick={() => navigate(`/response?dissId=${id}`)}
+    >
       <h1 className="text-4xl">{name}</h1>
       <h1 className="text-lg ">{diss}</h1>
+      <div className="flex items-end h-full">
+        <HeartIcon className="flex w-full h-12 px-2 justify-start" />
+        <div style={{ color: "red" }}>
+          <HeartIcon size={32} />
+        </div>
+        <ShareIcon className="flex w-full h-12 px-2 justify-start" />
+      </div>
     </div>
   );
 };
